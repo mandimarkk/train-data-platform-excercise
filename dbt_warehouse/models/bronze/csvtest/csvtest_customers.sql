e2add90d6e04f5fc9a2c5a8ee45a336acc6702cc
Mandi Markkanen <mandimarkkanen@kamk.fi>
2026-01-04 13:28:32 +0200

    added bronze models for fake data to dbt_warehouse

---
+++ dbt_warehouse/models/bronze/csvtest/csvtest_customers.sql
@@ -0,0 +1,2 @@
+SELECT "id", "name", "email"
+FROM read_csv('../data/lake/staging/csvtest/customers.csv')
---
+++ dbt_warehouse/models/bronze/csvtest/csvtest_orders.sql
@@ -0,0 +1,2 @@
+SELECT "id", "customer_id", "product", "quantity"
+FROM read_csv('../data/lake/staging/csvtest/orders.csv')
